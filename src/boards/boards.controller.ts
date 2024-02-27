7f0c7cfcd22b63457c0fb2e1bf714bbea5fd25da
JungSik Jeong <wndtlr1024@gmail.com>
2024-02-27 18:56:36 +0900

    .

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { BoardsService } from './boards.service';
+import { Board } from './board.modal';
+// 1 브라우저에서 /boards 에 요청을 보냄
 
+// 2 컨트롤러가 받음 ('/boards')
 @Controller('boards')
-export class BoardsController {}
+export class BoardsController {
+  constructor(private boardService: BoardsService) {}
+
+  // 3 get요청을 받음
+  @Get('/')
+  getAllBoard(): Board[] {
+    return this.boardService.getAllBoards();
+  }
+}
--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,4 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { Board } from './board.modal';
 
 @Injectable()
-export class BoardsService {}
+export class BoardsService {
+  private boards: Board[] = []; // 다른 컴포넌트에서 boards계열 값을 수정 못하게 private를 사용함
+
+  getAllBoards(): Board[] {
+    return this.boards;
+  }
+}
